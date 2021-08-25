f3ed05dff36143127a71e1bc9d0c144963260655
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-08-25 08:18:30 +0530

    op

--- 195A.cpp
+++ 195A.cpp
@@ -6,6 +6,12 @@ int main()
 {
     int a, b, c;
     cin >> a >> b >> c;
+    const int total = a * c;
+    const int can_come = b * c;
+    const int needed = total - can_come;
+
+    const int time_required = (needed + b - 1) / b;
+    cout << time_required << '\n';
 
     return 0;
 }
--- 886B.cpp
+++ 886B.cpp
@@ -7,6 +7,27 @@ using namespace std;
 
 int main()
 {
-    
+    int size;
+    int values[200001];
+    int indexes[200001] = {0};
+    cin >> size;
+
+    for (int i = 0; i < size; i++)
+        cin >> values[i];
+    int temp;
+    vector<int> results;
+
+    for (int i = size - 1; i > -1; i--)
+    {
+        temp = values[i];
+        indexes[temp]++;
+        if (indexes[temp] == 1)
+        {
+            results.push_back(temp);
+        }
+    }
+
+    cout << results[results.size() - 1] << '\n';
+
     return 0;
 }
