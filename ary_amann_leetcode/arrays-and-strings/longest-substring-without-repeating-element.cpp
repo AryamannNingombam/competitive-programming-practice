1d6c4009bb33941f60f9ad94e9096688038104e8
ary_amann <aryamannsingh9@gmail.com>
2022-02-16 15:58:18 +0530

    my merge intervals from last year added

--- ary_amann_leetcode/arrays-and-strings/longest-substring-without-repeating-element.cpp
+++ ary_amann_leetcode/arrays-and-strings/longest-substring-without-repeating-element.cpp
@@ -7,6 +7,31 @@ public:
         if (s.size() == 0)
             return 0;
         count[s[0]]++;
+        class Solution
+        {
+        public:
+            vector<vector<int>> merge(vector<vector<int>> &intervals)
+            {
+                int pointer = 1;
+                sort(intervals.begin(), intervals.end());
+                vector<vector<int>> result;
+                result.push_back(intervals[0]);
+                while (pointer < intervals.size())
+                {
+                    if (result.back()[1] >= intervals[pointer][0])
+                    {
+                        if (result.back()[1] < intervals[pointer][1])
+                            result.back()[1] = intervals[pointer][1];
+                    }
+                    else
+                    {
+                        result.push_back(intervals[pointer]);
+                    }
+                    pointer++;
+                }
+                return result;
+            }
+        };
         int answer{1};
         int i{}, j{};
         while (j != s.size() - 1)
