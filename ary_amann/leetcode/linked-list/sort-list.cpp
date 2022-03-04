6bc09f34bcf2e20f17010dfb67db55c996a81b11
ary_amann <aryamannsingh9@gmail.com>
2022-03-04 08:24:18 +0530

    sort list merge-sort

--- ary_amann/leetcode/linked-list/sort-list.cpp
+++ ary_amann/leetcode/linked-list/sort-list.cpp
@@ -11,8 +11,51 @@ struct ListNode
 
 class Solution
 {
+    ListNode *return_mid(ListNode *head)
+    {
+        ListNode *fast = head;
+        ListNode *slow = nullptr;
+        while (fast && fast->next)
+        {
+            slow = !slow ? head : slow->next;
+            fast = fast->next->next;
+        }
+        auto temp = slow->next;
+        slow->next = nullptr;
+        return temp;
+    }
+    ListNode *merge(ListNode *l, ListNode *r)
+    {
+        ListNode *head = new ListNode(0);
+        ListNode *cur = head;
+        while (l && r)
+        {
+            if (l->val < r->val)
+            {
+                cur->next = l;
+                l = l->next;
+            }
+            else
+            {
+                cur->next = r;
+                r = r->next;
+            }
+            cur = cur->next;
+        }
+        if (l)
+            cur->next = l;
+        if (r)
+            cur->next = r;
+        return head->next;
+    }
+
 public:
     ListNode *sortList(ListNode *head)
     {
+        if (!head || !head->next)
+            return head;
+        ListNode *mid = return_mid(head);
+        ListNode *l = head, *r = mid;
+        return merge(sortList(l), sortList(r));
     }
 };
