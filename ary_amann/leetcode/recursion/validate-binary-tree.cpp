f96ce0b4374e17ab24d3226521f7fb389e500856
ary_amann <aryamannsingh9@gmail.com>
2022-03-10 09:22:14 +0530

    validate binary tree own

--- ary_amann/leetcode/recursion/validate-binary-tree.cpp
+++ ary_amann/leetcode/recursion/validate-binary-tree.cpp
@@ -1,50 +1,44 @@
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution
 {
+    bool check_greater(TreeNode *root, TreeNode *parent)
+    {
+        if (!root)
+            return true;
+        if (root->val >= parent->val)
+            return false;
+        return check_greater(root->left, parent) && check_greater(root->right, parent);
+    }
+    bool check_smaller(TreeNode *root, TreeNode *parent)
+    {
+        if (!root)
+            return true;
+        if (root->val <= parent->val)
+            return false;
+        return check_smaller(root->left, parent) && check_smaller(root->right, parent);
+    }
+
 public:
     bool isValidBST(TreeNode *root)
     {
-        if (root == NULL)
-        {
+        if (!root)
             return true;
-        }
-        bool z = true, w = true;
-        if (root->left != NULL)
-        {
-            z = check(root->left, root->val, false);
-        }
-        if (root->right != NULL)
-        {
-            w = check(root->right, root->val, true);
-        }
-        bool x = isValidBST(root->left);
-        bool y = isValidBST(root->right);
-        return x && y && z && w;
-    }
-    /*Parentval is the value of root node with respect to which all values of left or right sub tree nodes are to be checked*/
-    bool check(TreeNode *root, int Parentval, bool val)
-    {
-        if (root == NULL)
-        {
-            return true;
-        }
-        if (val)
-        {
-            if (root->val <= Parentval)
-            {
-                return false;
-            }
-        }
-        else
-        {
-            if (root->val >= Parentval)
-            {
-                return false;
-            }
-        }
-        /*to check whether all nodes of left-sub tree have smaller value or not*/
-        bool q = check(root->left, Parentval, val);
-        /*to check whether all nodes of right-sub tree have greater value or not*/
-        bool w = check(root->right, Parentval, val);
-        return q && w;
+        bool l = true, r = true;
+        if (root->left)
+            l = check_greater(root->left, root);
+        if (root->right)
+            r = check_smaller(root->right, root);
+        return l && r && isValidBST(root->left) && isValidBST(root->right);
     }
 };
