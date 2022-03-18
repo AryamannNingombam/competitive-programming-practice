c229af4ff11cc5774db0caefbe2cdd8499cb4541
ary_amann <aryamannsingh9@gmail.com>
2022-03-18 23:27:40 +0530

    kth smallest wtf i knew this

--- ary_amann/leetcode/trees-and-graphs/kth-smallest-element.cpp
+++ ary_amann/leetcode/trees-and-graphs/kth-smallest-element.cpp
@@ -13,27 +13,20 @@
 //@TODO - wtf is this question
 class Solution
 {
-    int counter = 0;
-    int helper(TreeNode *root, int k, int counter = -1)
+    vector<int> result;
+    void helper(TreeNode *root, int k)
     {
-        if (root == nullptr)
-        {
-            counter = 1;
-            return counter;
-        }
-        if (counter == k)
-            return root->val;
-        int left = helper(root->left, k, counter);
-        if (left == k)
-            return root->val;
-        int right = helper(root->right, k, left);
-        if (right == k)
-            return root->val;
+        if (!root)
+            return;
+        helper(root->left, k);
+        result.push_back(root->val);
+        helper(root->right, k);
     }
 
 public:
     int kthSmallest(TreeNode *root, int k)
     {
-        return helper(root, k);
+        helper(root, k);
+        return result[k - 1];
     }
 };
