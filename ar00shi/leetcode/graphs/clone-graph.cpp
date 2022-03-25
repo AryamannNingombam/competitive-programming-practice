78b36ab1950cb812778eaf4a9dd2d14834a458a7
Arushi Gandhi <Arushi.gandhi@gmail.com>
2022-03-25 07:27:37 +0530

    ion get graphs

---
+++ ar00shi/leetcode/graphs/clone-graph.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node* , Node*> hash; 
+    Node* cloneGraph(Node* node) {
+        if(node == NULL) 
+        {
+            return NULL;
+        }
+        if(hash.find(node) == hash.end())
+        {
+            hash[node] = new Node(node -> val, {}); 
+            
+            for(auto adj: node -> neighbors)
+            {
+                hash[node] -> neighbors.push_back(cloneGraph(adj));
+            }
+        }
+        
+        return hash[node]; 
+    }
+};
+
+//dont fully get it
